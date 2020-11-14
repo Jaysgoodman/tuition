7b37afca9fa723a4cfbb17f03310f3a5e5dcd16c
Jaysgoodman <33941025+Jaysgoodman@users.noreply.github.com>
2020-11-15 01:32:04 +0800

    level

---
+++ level/index.php
@@ -0,0 +1,184 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Labuan Tuition Centre</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="" />
+
+<link href="css/font-awesome.css" rel="stylesheet"> 
+
+<script src="js/jquery-2.1.4.min.js"></script>
+
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+<style>
+* {box-sizing: border-box;}
+
+body { 
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  overflow: hidden;
+  background-color: black;
+  padding: 20px 10px;
+}
+
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
+  }
+}
+</style>
+
+</head>
+<body>
+    
+<div class="header">
+  <h1><a href="#default" >Labuan Tuition Centre</a></h1>
+  <div class="header-right">
+    <a href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+    
+    <h1>category</h1>
+<div class="wthree-dot">
+	
+	<div class="wrap">
+		<div class="w3l-pricing-grids">
+			<div class="agileits-pricing-grid">
+				<div class="pricing">
+					<div class="pricing-top">
+						<h3>UPSR</h3>
+					</div>
+					<div class="pricing-bottom">
+						<div class="pricing-bottom-top">
+							<p>registration fee</p>
+							<p>as a member</p>
+							<p>for rm20 only</p>
+							<p>once in a lifetime</p>
+							
+						</div>
+						<div class="pricing-bottom-bottom">
+							<p><span>RM115</span> 2 subjects</p>
+							<p><span>RM170 </span> 3 subjects</p>
+							<p><span>2 hours</span> class</p>  
+							<p class="text"><span>4-5 times</span> a week</p>
+						</div>
+						<div class="wthree-pricing-info">
+							<p>RM<span>60</span>/subject</p>
+						</div>
+						<div class="buy-button">
+							<a href="subjects/index.php">Next</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="agileits-pricing-grid">
+				<div class="pricing">
+					<div class="pricing-top blue-top">
+						<h3>PT3</h3>
+					</div>
+					<div class="pricing-bottom">
+						<div class="pricing-bottom-top blue-pricing-bottom-top">
+							<p>registration fee</p>
+							<p>as a member</p>
+							<p>for rm20 only</p>
+							<p>once in a lifetime</p>
+						</div>
+						<div class="pricing-bottom-bottom blue-pricing-bottom-top">
+							<p><span>RM155</span> 2 subjects</p>
+							<p><span>RM230 </span> 3 subjects</p>
+							<p><span>2 hours</span> class</p>  
+							<p class="text"><span>4-5 times</span> a week</p>
+							
+						</div>
+						<div class="wthree-pricing-info blue-top">
+							<p>RM<span>80</span>/subject</p>
+						</div>
+						<div class="buy-button">
+							<a href="subjects2/index.php">Next</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="agileits-pricing-grid">
+				<div class="pricing">
+					<div class="pricing-top green-top">
+						<h3>SPM</h3>
+					</div>
+					<div class="pricing-bottom">
+						<div class="pricing-bottom-top green-pricing-bottom-top">
+							<p>registration fee</p>
+							<p>as a member</p>
+							<p>for rm20 only</p>
+							<p>once in a lifetime</p>
+						</div>
+						<div class="pricing-bottom-bottom green-pricing-bottom-top">
+							<p><span>RM195</span> 2 subjects</p>
+							<p><span>RM290 </span> 3 subjects</p>
+							<p><span>2 hours</span> class</p>  
+							<p class="text"><span>4-5 times</span> a week</p>
+							
+						</div>
+						<div class="wthree-pricing-info green-top">
+							<p>RM<span>100</span>/subject</p>
+						</div>
+						<div class="buy-button">
+							<a  href="subjects3/index.php">Next</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clear"> </div>
+		</div>
+		
+		
+	</div>
+</div>
+</body>
+</html>
